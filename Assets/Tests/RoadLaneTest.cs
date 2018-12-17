a8d6ab26d2e6a013ce45b52ca9e6d0bd24ffa57f
Toni Alatalo <toni@playsign.net>
2018-12-17 21:33:01 +0200

    simple addition of car hooks to test setup. not used yet. tweaks to car lane: has margins so does not drive to overlap with bike lane. also an additional non-moving bike in a corner to make bike avoid the bike lane

--- Assets/Tests/RoadLaneTest.cs
+++ Assets/Tests/RoadLaneTest.cs
@@ -67,23 +67,34 @@ namespace Tests
             // Use yield to skip a frame.
             
             //actor & target
-            GameObject a = null;
-            GameObject t = null;
-            while (a == null || t == null) {
-                a = GetActor();
-                t = GetTarget();
+            //Bike & Target
+            GameObject b = null;
+            GameObject bt = null;
+            //Car & Target
+            GameObject c = null;
+            GameObject ct = null;
+            while (b == null || bt == null || c == null || ct == null) {
+            //doesn't really make sense cause these all come at scene load. don't know if could make sense with some open data dataset or other test conf loading
+            //anyhow this actually makes it somehow clear that is about load wait / test dependencies:
+                b = GameObject.Find("Biker");
+                bt = GameObject.Find("BikeTarget"); //there is also 2 behind the corner, not used yet (would work for this test too)
+
+                c = GameObject.Find("Car");
+                ct = GameObject.Find("CarTarget3"); //one for the complete test with lotsa turns with also bike lane on the side & then crossing the same road behind and the lane to other dir, with NavMesh Link
+
                 yield return false;
             }
-            Debug.Log($"{a} -> {t}");
+            Debug.Log($"{b.name} : {b} -> {bt}");
+            Debug.Log($"{c.name} : {c} -> {ct}");
 
             while (true) {
-                NavMeshAgent agent = a.GetComponent<NavMeshAgent>();
+                NavMeshAgent agent = b.GetComponent<NavMeshAgent>();
                 NavMeshPath path = agent.path;
                 Debug.Log(path);
                 Vector3[] corners = path.corners;
                 Debug.Log(corners);
 
-                float dist = Vector3.Distance(a.transform.position, t.transform.position);
+                float dist = Vector3.Distance(b.transform.position, bt.transform.position);
                 Debug.Log(dist);
                 if (dist < 0.1f) { //with close position of the origins in y dir goes to 0.007901428 now - so this has plenty margin. 10cm is considered 'there' in this traffic nav
                     break;
@@ -94,10 +105,12 @@ namespace Tests
         }
 
         //next / complex test? .. lanes and intersection?
-        //or is something else more meaningful?
-        //setting lane widths?
+        //DONE now with surface types & NavMesh Link in the test scene - test does not check yet that the lane biz works, but it shows visually
 
         //one test for car: do not cross bike path! (in wrong place / vain, at least?) .. maybe useful to see lanes go sensible, or actually should just check the surface but.. eh?
+
+        //or is something else more meaningful?
+        //setting lane widths?        
 
 /*
         public bool IsTestFinished
